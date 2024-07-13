f609c7103f52827d72f04932cc7b95c4f067f27f
Mhd Basel Bashir <u13472@isoint.net>
2024-07-13 23:36:11 +0300

    first commit

---
+++ gateway/src/main/java/com/safalifter/gateway/config/GatewayConfig.java
@@ -0,0 +1,29 @@
+package com.safalifter.gateway.config;
+
+import com.safalifter.gateway.filter.JwtAuthenticationFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    private final JwtAuthenticationFilter filter;
+
+    public GatewayConfig(JwtAuthenticationFilter filter) {
+        this.filter = filter;
+    }
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-service", r -> r.path("/user/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("http://localhost:8081"))
+
+                .route("auth-service", r -> r.path("/auth/**")
+                        .uri("http://localhost:8082"))
+
+                .build();
+    }
+}
